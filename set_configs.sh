34e9bf4da4ec8ae0f77fbc7a9724469378ec8a73
NickPerrin <perrin.64@osu.edu>
2019-11-10 10:29:00 -0500

    added script to set system configs to repo version

---
+++ set_configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set each of the config files known by this repo
+shopt -s dotglob 
+echo "updating files"
+
+# Copy config files from the repo to the system 
+for file in mappings/*; do
+    filename="$(basename $file)"
+    cp -a ./$filename $file
+done
+
+shopt -u dotglob 
