b4acfe20c05466cff5ceb4f231c61d013779d7de
ppusher <happydayz4ever2@gmail.com>
2025-12-30 17:10:01 -0700

    add fish configs

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+alias tls "tmux ls"
+alias tkill "tmux kill-session -t"
+alias tattach "tmux attach-session -t"
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
